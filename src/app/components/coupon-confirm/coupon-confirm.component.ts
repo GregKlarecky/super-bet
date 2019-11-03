1c837c46cf806361fef5e098731459306dbcdd23
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2019-11-03 08:46:58 +0100

    coupon-confirm animation

--- src/app/components/coupon-confirm/coupon-confirm.component.ts
+++ src/app/components/coupon-confirm/coupon-confirm.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { ModalService } from "src/app/services/modal.service";
+import { fade } from "src/app/animation/fade.animation";
+import { gentleDrop } from "src/app/animation/gantle-drop.animation";
+import { timer } from "rxjs";
 
 @Component({
   selector: "app-coupon-confirm",
   templateUrl: "./coupon-confirm.component.html",
-  styleUrls: ["./coupon-confirm.component.scss"]
+  styleUrls: ["./coupon-confirm.component.scss"],
+  animations: [gentleDrop, fade]
 })
 export class CouponConfirmComponent implements OnInit {
+  public show = true;
   constructor(private modalService: ModalService) {}
 
   ngOnInit() {}
   close() {
-    this.modalService.closePopup.next();
+    this.show = false;
+    timer(500).subscribe(() => {
+      this.modalService.closePopup.next();
+    });
   }
 }
